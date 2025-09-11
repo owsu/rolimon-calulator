afd1083c0dd241544ca852f2a8f3e6183a6b7402
Owusu Kantankah <139204333+owsu@users.noreply.github.com>
2025-09-10 22:08:03 -0500

    Added main and rolimons api file.
    main.py is where the user is prompted and interacts with the terminal
    rolimons.py uses the request module to read json data from the rolimons api to determine item data.

---
+++ main.py
@@ -0,0 +1,69 @@
+from rolimons import Rolimons
+import math
+
+# Simple Rolimons Trade Calculator
+rolimons = Rolimons()
+
+yourSideValue = 0
+theirSideValue = 0
+
+yourHighestDemand = 0
+theirHighestDemand = 0
+
+print("Items you are giving: (splice int with commas)")
+itemsGiving = input("")
+itemsGiving = itemsGiving.split(",")
+for id in itemsGiving:
+    id = int(id)
+    demandName, demandId = rolimons.getDemand(id)
+    if demandId > yourHighestDemand:
+        yourHighestDemand = demandId
+
+print("Items you are receiving: (splice int with commas)")
+itemsReceiving = input("")
+itemsReceiving = itemsReceiving.split(",")
+for id in itemsGiving:
+    id = int(id)
+    demandName, demandId = rolimons.getDemand(id)
+    if demandId > theirHighestDemand:
+        theirHighestDemand = demandId
+
+
+
+for id in itemsGiving:
+    yourSideValue += rolimons.getValue(id)
+
+for id in itemsReceiving:
+    theirSideValue += rolimons.getValue(id)
+
+isUpgrading = (len(itemsGiving) > len(itemsReceiving)) # upgrading is the act of giving multiple small items for a bigger item
+
+for id in itemsGiving:
+    print(rolimons.displayStats(id))
+    print("----------------------")
+print(yourSideValue)
+print("==========FOR==========")
+
+for id in itemsReceiving:
+    print(rolimons.displayStats(id))
+    print("----------------------")
+print(theirSideValue)
+
+if isUpgrading: # if we are upgrading then losing up to 5% is fine
+    print("You are upgrading")
+    calculatedRate = 5 / (1 + (theirHighestDemand/100))
+    
+    if (yourSideValue/theirSideValue <= calculatedRate):
+        print("Adequate trade")
+    else:
+        print("You are overpaying too much")
+else: # if we are downgrading then we need a gain that slightly decreases the more expensive the item is, but increases based off demand
+    print("You are downgrading")
+    calculatedRate = (1/ (2*math.log(yourSideValue, 10))) * (1+ (yourHighestDemand/100))
+
+    if (yourSideValue/theirSideValue >= calculatedRate):
+        print("Adequate trade")
+    else:
+        print("You are getting lowballed")
+     
+
---
+++ rolimons.py
@@ -0,0 +1,43 @@
+import requests
+
+class Rolimons():
+
+    def __init__(self):
+        catalog = requests.get("https://www.rolimons.com/itemapi/itemdetails")
+        self.items = catalog.json()["items"]
+
+    def getDemand(self, id: int):
+        demandChart = {-1: "None", 0: 'terrible', 1: 'low', 2: 'normal', 3: 'high', 4: 'amazing'}
+        if str(id) not in self.items:
+            return -1
+        
+        itemDemand = self.items[str(id)][5]
+        return demandChart[itemDemand], itemDemand
+
+    def getValue(self, id: int):
+        if str(id) not in self.items:
+            return -1
+        
+        itemValue = self.items[str(id)][4]
+        return itemValue
+    
+    def isProjected(self, id: int):
+        if str(id) not in self.items:
+            return -1
+        
+        isProj = (1 == self.items[str(id)][7])
+        return isProj
+    
+    def toName(self, id: int):
+        if str(id) not in self.items:
+            return "None"
+
+        name = self.items[str(id)][0]
+        return name
+    
+    def displayStats(self, id: int):
+        print("Item: ", self.toName(id))
+        print("Projected: ", self.isProjected(id))
+        print("Value: ", self.getValue(id))
+        print("Demand: ", self.getDemand(id))
+
